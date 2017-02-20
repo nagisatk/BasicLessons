ca314a7da6b13fd6b768728a3c054a7b95e0e0c6
Nagisa <nagisa_tk@outlook.com>
2017-02-20 11:37:16 +0800

    modified

---
+++ TCPL/chapter1.1-Exercise.1-2.c
@@ -0,0 +1,37 @@
+/*****************************************************
+ >    File Name: chapter1.1-Exercise.1-2.c
+ >  Author Name: Nagisa
+ > Mail Address: nagisa_tk@outlook.com
+ > Created Time: 2017/2/16 20:40:00
+ *****************************************************/
+
+#include<stdio.h>
+
+int main() {
+    printf("\a");
+    printf("\b");
+    // printf("\c");
+    // printf("\d");
+    printf("\e");
+    printf("\f");
+    // printf("\g");
+    // printf("\h");
+    // printf("\i");
+    // printf("\j");
+    // printf("\k");
+    // printf("\l");
+    // printf("\m");
+    printf("\n");
+    // printf("\o");
+    // printf("\p");
+    // printf("\q");
+    printf("\r");
+    // printf("\s");
+    printf("\t");
+    printf("\u2345");   // unicode
+    printf("\v");
+    // printf("\w");
+    printf("\xffff");   // hex
+    // printf("\y");
+    // printf("\z");
+}
--- TCPL/chapter1.1.c
+++ TCPL/chapter1.1.c
@@ -9,31 +9,4 @@
 
 int main() {
     printf("hello, world\n");
-    // Exercise 1-2
-    printf("\a");
-    printf("\b");
-    // printf("\c");
-    // printf("\d");
-    printf("\e");
-    printf("\f");
-    // printf("\g");
-    // printf("\h");
-    // printf("\i");
-    // printf("\j");
-    // printf("\k");
-    // printf("\l");
-    // printf("\m");
-    printf("\n");
-    // printf("\o");
-    // printf("\p");
-    // printf("\q");
-    printf("\r");
-    // printf("\s");
-    printf("\t");
-    printf("\u2345");   // unicode
-    printf("\v");
-    // printf("\w");
-    printf("\xffff");   // hex
-    // printf("\y");
-    // printf("\z");
 }
