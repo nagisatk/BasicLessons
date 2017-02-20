5cd63fcac986708120549e399a1de7a286103307
Nagisa <nagisa_tk@outlook.com>
2017-02-20 11:39:27 +0800

    modified

---
+++ TCPL/chapter1.1-1.c
@@ -0,0 +1,12 @@
+/*****************************************************
+ >    File Name: chapter1.1.c
+ >  Author Name: Nagisa
+ > Mail Address: nagisa_tk@outlook.com
+ > Created Time: 2017/2/16 20:23:06
+ *****************************************************/
+
+#include<stdio.h>
+
+int main() {
+    printf("hello, world\n");
+}
