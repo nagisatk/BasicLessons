d0fa9e69cee68fe5734524672c25b744c5565956
Nagisa <nagisa_tk@outlook.com>
2017-11-21 21:09:27 +0800

    sicp chapter 1.1.8

---
+++ sicp/Chapter 1/sicp-ch-1.1.8.scm
@@ -0,0 +1,21 @@
+#lang scheme
+
+; Chapter 1.1.8
+; Internal definitions and block structure
+(define (average x y)
+  (/ (+ x y) 2))
+(define (square x)
+  (* x x))
+
+(define (sqrt x)
+  (define (good-enough? guess)
+    (< (abs (- (square guess) x)) 0.001))
+  (define (improve guess)
+    (average guess (/ x guess)))
+  (define (sqrt-iter guess)
+    (if (good-enough? guess)
+      guess
+      (sqrt-iter (improve guess))))
+  (sqrt-iter 1.0))
+
+(sqrt 2)
